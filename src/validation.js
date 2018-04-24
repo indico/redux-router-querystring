c159611d50ad3d257d68e31f6d54b0cc31b33745
Adrian Moennich <adrian.moennich@cern.ch>
2018-04-24 12:36:02 +0200

    Avoid using Proxy
    IE11... :see_no_evil:

--- src/validation.js
+++ src/validation.js
@@ -2,16 +2,18 @@ import moment from 'moment';
 import valid from 'validator';
 import setDeep from 'lodash.set';
 
-// Proxy object that allows for partial validator functions
-// e.g. validator.isIn(['a', 'b'])(val)
-// It also implements a few useful utility methods
-const _validator = new Proxy({
-    isDate: str => moment(str, 'YYYY-MM-DD').isValid(),
-    isTime: str => moment(str, 'HH:mm').isValid()
-}, {
-    get: (obj, prop) => (...options) => str => ((obj[prop] ? obj[prop] : valid[prop])(str, ...options))
-});
-export {_validator as validator};
+
+const funcs = Object.entries(valid)
+    .filter(([, fn]) => typeof fn === 'function')
+    .map(([key, fn]) => {
+        return {[key]: (...options) => (str) => fn(str, ...options)};
+    });
+
+export const validator = Object.assign({
+    isDate: () => str => moment(str, 'YYYY-MM-DD').isValid(),
+    isTime: () => str => moment(str, 'HH:mm').isValid()
+}, ...funcs);
+
 
 export const mapQueryFields = (fields, mapping) => {
     const data = {};
